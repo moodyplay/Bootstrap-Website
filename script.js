3574ebc774365b05b77a5418f246807ad943ff8d
PatrykHensa <patrykhensa@gmail.com>
2023-11-21 21:08:39 +0100

    add navbar autohide when link pressed

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 document.addEventListener("DOMContentLoaded", function(){
     const el_autohide = document.querySelector('.navbar-autohide');
+    const el_showCase = document.getElementById("container");
+    const menuToggle = document.getElementById('navmenu')
+    const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+
+    navbar_height = document.querySelector('.navbar').offsetHeight;
+    el_showCase.style.paddingTop = navbar_height + 'px';
+    
+    
+
 
     if (el_autohide) {
         let last_scroll_top = 0;
@@ -8,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function(){
             if(scroll_top < last_scroll_top){
                 el_autohide.classList.remove('scrolled-down');
                 el_autohide.classList.add('scrolled-up');  
+                bsCollapse.hide();
             } else {
+                bsCollapse.hide();
+                
                 el_autohide.classList.remove('scrolled-up');
                 el_autohide.classList.add('scrolled-down');
             }
