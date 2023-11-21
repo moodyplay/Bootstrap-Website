521344e597a8470313c108b9490ffb9bfd57ca2f
PatrykHensa <patrykhensa@gmail.com>
2023-11-21 21:19:31 +0100

    fix autohide doesnt work at mobile

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
     const el_autohide = document.querySelector('.navbar-autohide');
     const el_showCase = document.getElementById("container");
     const menuToggle = document.getElementById('navmenu')
-    const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+    const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false })
 
     navbar_height = document.querySelector('.navbar').offsetHeight;
     el_showCase.style.paddingTop = navbar_height + 'px';
-    
-    
-
 
     if (el_autohide) {
         let last_scroll_top = 0;
-        window.addEventListener('scroll', function(){
+        let isNavbarOpen = false;
+
+        window.addEventListener('scroll', function () {
             let scroll_top = window.scrollY;
-            if(scroll_top < last_scroll_top){
+            if (scroll_top < last_scroll_top) {
                 el_autohide.classList.remove('scrolled-down');
-                el_autohide.classList.add('scrolled-up');  
-                bsCollapse.hide();
+                el_autohide.classList.add('scrolled-up');
+                if (isNavbarOpen) {
+                    bsCollapse.hide();
+                    isNavbarOpen = false;
+                }
             } else {
-                bsCollapse.hide();
-                
+                if (isNavbarOpen) {
+                    bsCollapse.hide();
+                    isNavbarOpen = false;
+                }
+
                 el_autohide.classList.remove('scrolled-up');
                 el_autohide.classList.add('scrolled-down');
             }
             last_scroll_top = scroll_top;
+        });
+
+        
+        window.addEventListener('touchstart', function (e) {
+            
+            if (isNavbarOpen && !menuToggle.contains(e.target)) {
+                bsCollapse.hide();
+                isNavbarOpen = false;
+            }
+        });
+
+        
+        menuToggle.addEventListener('show.bs.collapse', function () {
+            isNavbarOpen = true;
+        });
+
+        
+        menuToggle.addEventListener('hidden.bs.collapse', function () {
+            isNavbarOpen = false;
         });
     }
 });
